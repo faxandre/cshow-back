3aafdb1cd4ef4d42019cdbb93c26ca8478b97713
Andre <faxandre@gmail.com>
2022-11-03 11:56:36 -0400

    .env

--- src/config/db.js
+++ src/config/db.js
@@ -4,20 +4,6 @@ if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-
-//LOCAL MYSQL
-/*const sequelize = new Sequelize(
-    'cshow',//database
-    'root',//user
-    '',//pass
-    {
-        dialect: 'mysql',//dialeto
-        host: 'localhost',//host mongo()
-        port: 3306 //porta 
-    }
-);*/
-
-
 /**
  * MySQL HEROKU
  * STRING CONNECTION: mysql://b74010a43b9c1d:afc5aa99@us-cdbr-east-06.cleardb.net/heroku_f6661ce4fa3fa9b?reconnect=true
@@ -27,9 +13,6 @@ if(process.env.NODE_ENV !== 'production') {
  * PASS: afc5aa99
  * DATABASE: heroku_f6661ce4fa3fa9b
  * */
-//console.log(process.env.NODE_ENV);
-//console.log(process.env.DB_SCHEMA);
-//console.log(process.env.DB_PORT);
 const sequelize = new Sequelize(
     process.env.DB_SCHEMA,//database
     process.env.DB_USER,//user
@@ -37,7 +20,7 @@ const sequelize = new Sequelize(
     {
         dialect: process.env.DB_DIALECT,
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        //port: process.env.DB_PORT
     }
 );
 /*const sequelize = new Sequelize(
@@ -50,7 +33,17 @@ const sequelize = new Sequelize(
         //port: 3306
     }
 );*/
-
+//LOCAL MYSQL
+/*const sequelize = new Sequelize(
+    'cshow',//database
+    'root',//user
+    '',//pass
+    {
+        dialect: 'mysql',//dialeto
+        host: 'localhost',//host mongo()
+        port: 3306 //porta 
+    }
+);*/
 
 
 
