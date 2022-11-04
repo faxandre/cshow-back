1d5f120e845c26352a4589b6805725dd0603f0ad
Andre <faxandre@gmail.com>
2022-11-04 00:41:19 -0400

    adicionando imagens e dump do banco

--- README.md
+++ README.md
@@ -1,41 +1,79 @@
 # cshow-back
 CSHOW - Backend de uma app de cadastro e listagem de filmes (NODE 16.14.0)
+<br><br>
+
+# Clonando o Projeto 
+```sh
+git clone https://github.com/faxandre/cshow-back.git
+```
+<br><br>
 
 
-<h1 align="center">CShow</h1>
-<h3 align="center">Sistema de crud para locadora de filmes</h3>
+# Instalando as Dependências 
+```sh
+yarn install
+```
+<br><br>
+
+# Executar o Projeto Localmente 
+```sh
+yarn dev
+```
+<br><br>
+
+# DEV - URL Local
+http://localhost:8888/api/v1/
+<br><br>
+
+# PROD - URL Heroku
+https://cshow-back.herokuapp.com/api/v1/
+<br><br>
+
+# Instalando Libs Individuais se Necessário
+## Dependência do projeto
+```sh
+yarn add express
+yarn add cors
+yarn add mysql2
+yarn add sequelize
+yarn add multer
+yarn add yup
+yarn add dotenv
+yarn add swagger-ui-express
+```
+## Dependência somente no ambiente de desenvolvimento
+```sh
+yarn add nodemon -D
+yarn add sequelize-cli -D
+yarn add @types/swagger-ui-express -D
+```
+<br><br>
 
 
-<h3 align="left">Languages and Tools:</h3>
+<h3 align="left">Stack</h3>
 <p align="left"> 
-    <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> 
-        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40"/> 
+    <a href="https://nodejs.org" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> 
+    </a>
+    <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> 
+    </a>     
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> 
+    </a>
+    <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> 
     </a> 
-    <a href="https://flask.palletsprojects.com/" target="_blank" rel="noreferrer"> 
-        <img src="https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg" alt="flask" width="40" height="40"/> 
-    </a> 
+    <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> 
+        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> 
+    </a>
     <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> 
         <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40"/> 
     </a> 
     <a href="https://heroku.com" target="_blank" rel="noreferrer"> 
         <img src="https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg" alt="heroku" width="40" height="40"> 
     </a> 
-    <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> 
-        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> 
-    </a> 
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> 
-        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> 
-    </a> 
-    <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> 
-        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/> 
-    </a> 
-    <a href="https://nodejs.org" target="_blank" rel="noreferrer"> 
-        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> 
-    </a> 
     <a href="https://postman.com" target="_blank" rel="noreferrer"> 
         <img src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" alt="postman" width="40" height="40"/> 
-    </a> 
-    <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> 
-        <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> 
-    </a> 
+    </a>
 </p>
---
+++ database/dump_database.sql
@@ -0,0 +1,48 @@
+/*
+-- Query: Create Table
+-- Date: 2022-11-04 00:17
+*/
+CREATE TABLE IF NOT EXISTS `movie` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `img` varchar(50) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=394 DEFAULT CHARSET=utf8
+
+/*
+-- Query: SELECT * FROM movie LIMIT 0, 50000
+-- Date: 2022-11-04 00:17
+*/
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (54,'Rambo III','Desrição do Rambo3','1667509551416.png','2022-11-02 18:22:02','2022-11-03 21:05:52');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (64,'Velozes e Furiosos 2','DescricaoVelozes e Furiosos 2','1667413329137.png','2022-11-02 18:22:09','2022-11-02 18:22:09');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (74,'O dia em que a terra parou','Descricao O dia em que a terra parou','1667413330924.png','2022-11-02 18:22:10','2022-11-02 18:22:10');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (84,'Homem de Ferro 2','descricao Homem de Ferro 2','1667413332306.png','2022-11-02 18:22:12','2022-11-02 18:22:12');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (94,'A filha do chefe','Descricao A filha do chefe','1667413335722.png','2022-11-02 18:22:15','2022-11-02 18:22:15');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (104,'Smorfs','Descricao Smorfs','1667413337602.png','2022-11-02 18:22:17','2022-11-02 18:22:17');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (134,'Jurassic Park','Descricao Jurassic Park','1667493097886.png','2022-11-03 16:31:39','2022-11-03 16:31:39');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (144,'Duna','Descricao Duna','1667493105291.png','2022-11-03 16:31:46','2022-11-03 16:31:46');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (154,'Rambo 2','Descricao Rambo II','1667507278379.png','2022-11-03 16:31:50','2022-11-03 20:27:58');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (164,'Romeu tem que morrer','Descricao Romeu tem que morrer','1667494277861.png','2022-11-03 16:51:17','2022-11-03 16:51:17');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (174,'Romeu e Julieta','Descricao Romeu e Julieta','1667507278379.png','2022-11-03 16:51:30','2022-11-03 16:51:30');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (184,'Um tira da pesada','Descricao Um tira da pesada','1667507278379.png','2022-11-03 16:58:51','2022-11-03 16:58:51');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (194,'Aventureiros do bairro perdido','Decricao Aventureiros do bairro perdido','1667507278379.png','2022-11-03 17:03:19','2022-11-03 17:03:19');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (204,'Robocop','Descricao Robocop','1667507278379.png','2022-11-03 17:03:22','2022-11-03 17:03:22');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (224,'Duro de matar','Descricao Duro de matar','1667507278379.png','2022-11-03 18:54:45','2022-11-03 18:54:45');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (234,'Operação Dragão','Descricao Operação Dragão','1667507278379.png','2022-11-03 18:54:55','2022-11-03 18:54:55');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (244,'TopGun','Descricao topgun','1667507278379.png','2022-11-03 18:55:00','2022-11-03 18:55:00');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (254,'Stallone Cobra','Descricao Stallone Cobra','1667507278379.png','2022-11-03 18:55:02','2022-11-03 18:55:02');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (264,'Um principe em Nova York','Descricao Um principe em Nova York','1667507278379.png','2022-11-03 18:55:20','2022-11-03 18:55:20');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (274,'Footlose','Descricao Footlose','1667507278379.png','2022-11-03 18:55:29','2022-11-03 18:55:29');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (284,'Conan','Descricao Conan','1667507278379.png','2022-11-03 18:57:08','2022-11-03 18:57:08');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (294,'O pequeno mestre','Descricao O pequeno mestre','1667507278379.png','2022-11-03 18:59:56','2022-11-03 18:59:56');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (304,'Maquina mortifera','Desc Maquina mortifera','1667507278379.png','2022-11-03 19:04:13','2022-11-03 19:04:13');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (314,'Krull','Desc Krull','1667507278379.png','2022-11-03 19:04:14','2022-11-03 19:04:14');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (324,'O grande dragao branco','Desc O grande dragao branco','1667507278379.png','2022-11-03 19:04:25','2022-11-03 19:04:25');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (334,'MadMax','Desc Madmax','1667507278379.png','2022-11-03 20:28:01','2022-11-03 20:28:01');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (344,'Karate Kid','Desc Karatê Kid','1667507278379.png','2022-11-03 20:28:03','2022-11-03 20:28:03');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (364,'De volta para o futuro','Desc  De volta para o futuro','1667507278379.png','2022-11-03 21:05:41','2022-11-03 21:05:41');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (374,'Hellraiser - renascido do inferno ','Desc Hellraiser - renascido do inferno ','1667507278379.png','2022-11-04 02:35:28','2022-11-04 02:35:28');
+INSERT INTO `` (`id`,`title`,`description`,`img`,`createdAt`,`updatedAt`) VALUES (384,'Indiana Jones ','Desc Indiana Jones ','1667507278379.png','2022-11-04 02:39:17','2022-11-04 02:41:25');
